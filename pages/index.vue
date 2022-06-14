3416a31bce038a74fc30f343c73d5c33732d5b83
Danny Festor <danny.festor@gmail.com>
2022-06-14 17:27:38 +0900

    Update Index

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,11 @@
         class="absolute top-[50%] -translate-y-[50%] left-0 bottom-0 opacity-30"
     />
     <article class="relative flex flex-1 flex-col items-center justify-center gap-8 px-8">
-      <h2 class="text-center text-2xl uppercase">Hi, I'm Danny!</h2>
-      <h1 class="text-center text-4xl font-bold">I'm a <strong>Freelancer</strong></h1>
+      <h2 class="text-center text-2xl uppercase">Hi, ich bin Danny!</h2>
+      <h1 class="text-center text-4xl font-bold">Ich bin ein <strong>Freelancer</strong></h1>
       <p class="text-center text-xl">
-        I create<br /><strong>modern homepages</strong><br />and<br /><strong>web services</strong><br /> using
-        <em class="not-italic">the newest Technologies and Methodologies</em>
+        Ich erstelle<br /><strong>moderne Webseiten</strong><br />und<br /><strong>Web Services</strong><br /> mittels
+        <em class="not-italic">neuester Technologien und Methodologien</em>
       </p>
     </article>
   </section>
