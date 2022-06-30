899fa941e2aaa30c54cad4cb5ab71e7381f3432e
Danny Festor <danny.festor@gmail.com>
2022-06-30 15:38:27 +0900

    Add SEO Tags

--- layouts/jap.vue
+++ layouts/jap.vue
@@ -8,6 +8,32 @@
   </div>
 </template>
 
+<script setup>
+const route = useRoute()
+
+useHead({
+  title: 'ブリュッケ - 希望に沿ったモダンで高速なホームページを作成します',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'ブリュッケ - 希望に沿ったモダンで高速なホームページを作成します - フルスタック開発 - Laravel, GO, Node.js - Vue.js' },
+    { name: 'og:title', content: `希望に沿ったモダンで高速なホームページを作成します!` }, // ${route.meta.title} - via definePageMeta({ title: 'Some Page' })
+    { name: 'og:url', content: 'https://www.festor.info/ja' },
+    { name: 'og:description', content: 'ブリュッケ - 希望に沿ったモダンで高速なホームページを作成します - フルスタック開発 - Laravel, GO, Node.js - Vue.js' },
+    { name: 'og:image', content: 'https://www.festor.info/_nuxt/danny.a88332d3.jpg' },
+    { name: 'og:locale', content: 'ja-JP' },
+    { name: 'og:type', content: 'ポートフォリオ' },
+    { name: 'og:site_name', content: 'ブリュッケ' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@denakino' },
+    { name: 'twitter:creator', content: '@denakino' },
+    { name: 'twitter:title', content: `ブリュッケ - 希望に沿ったモダンで高速なホームページを作成します` },
+    { name: 'twitter:description', content: 'ブリュッケ - 希望に沿ったモダンで高速なホームページを作成します - フルスタック開発 - Laravel, GO, Node.js - Vue.js' },
+    { name: 'twitter:image', content: 'https://www.festor.info/_nuxt/danny.a88332d3.jpg' },
+  ],
+})
+</script>
+
 <style>
 html {
   scroll-behavior: smooth;
--- layouts/user.vue
+++ layouts/user.vue
@@ -8,6 +8,32 @@
   </div>
 </template>
 
+<script setup>
+const route = useRoute()
+
+useHead({
+  title: 'Brücke - Moderne und schnelle Homepages nach Ihren Wünschen!',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'Brücke - Moderne und schnelle Homepages nach Ihren Wünschen - Fullstack Development - Laravel, GO, Node.js - Vue.js' },
+    { name: 'og:title', content: `Moderne und schnelle Homepages nach Ihren Wünschen!` }, // ${route.meta.title} - via definePageMeta({ title: 'Some Page' })
+    { name: 'og:url', content: 'https://www.festor.info' },
+    { name: 'og:description', content: 'Brücke - Moderne und schnelle Homepages nach Ihren Wünschen - Fullstack Development - Laravel, GO, Node.js - Vue.js' },
+    { name: 'og:image', content: 'https://www.festor.info/_nuxt/danny.a88332d3.jpg' },
+    { name: 'og:locale', content: 'de-DE' },
+    { name: 'og:type', content: 'Portfolio' },
+    { name: 'og:site_name', content: 'Brücke' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@denakino' },
+    { name: 'twitter:creator', content: '@denakino' },
+    { name: 'twitter:title', content: `Brücke - Moderne und schnelle Homepages nach Ihren Wünschen!` },
+    { name: 'twitter:description', content: 'Brücke - Moderne und schnelle Homepages nach Ihren Wünschen - Fullstack Development - Laravel, GO, Node.js - Vue.js' },
+    { name: 'twitter:image', content: 'https://www.festor.info/_nuxt/danny.a88332d3.jpg' },
+  ],
+})
+</script>
+
 <style>
 html {
   scroll-behavior: smooth;
